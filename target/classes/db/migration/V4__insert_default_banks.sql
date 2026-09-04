d43f9b6763ca41ea6e09cdacea89e7eb1b3879bb
nguyen giang nam <nam@Nam-4.local>
2026-09-04 17:15:33 +0700

    chore: remove build artifacts and add .gitignore

--- target/classes/db/migration/V4__insert_default_banks.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO bank (code, name, website) VALUES
-('VCB', 'Vietcombank', 'https://portal.vietcombank.com.vn'),
-('BIDV', 'BIDV', 'https://bidv.com.vn'),
-('CTG', 'VietinBank', 'https://www.vietinbank.vn');
--- target/classes/db/migration/V5__insert_more_banks.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO bank (code, name, website) VALUES
-('TCB', 'Techcombank', 'https://techcombank.com'),
-('MB', 'MBBank', 'https://mbbank.com.vn'),
-('VPB', 'VPBank', 'https://vpbank.com.vn'),
-('ACB', 'ACB', 'https://acb.com.vn'),
-('STB', 'Sacombank', 'https://sacombank.com.vn');
